6f533fa2342f18a91e248600761deae564c27cd8
Paul Raucci <paulraucci@Pauls-MacBook-Pro.local>
2019-10-16 10:55:27 -0400

    maps up!

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
-console.log("connected");
+let map, infoWindow;
 
-let map;
-initMap();
-const options = {
-  center: { lat: 41.043149, lng: -73.532469 },
-  zoom: 10
-};
-map = new google.maps.Map(document.getElementById("map"), options);
+function initMap() {
+  let options = {
+    center: { lat: 41.043042, lng: -73.53257 },
+    zoom: 15
+  };
+  map = new google.maps.Map(document.getElementById("map"), options);
+
+  infoWindow = new google.maps.InfoWindow();
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(p) {
+      let position = {
+        lat: p.coords.latitude,
+        lng: p.coords.longitude
+      };
+      infoWindow.setPosition(position);
+      infoWindow.setContent("You are here");
+      infoWindow.open(map);
+    });
+  } else {
+    handleLocationError("No geolocation", map.center());
+  }
+
+  function handleLocationError(content, position) {
+    infoWindow.setPosition(position);
+    infoWindow.setContent(content);
+    infoWindow.open(map);
+  }
+}
